d5fb1be1e977ccf1d0fb5fa400d758f122bc5589
sky <452425952@qq.com>
2021-07-08 22:49:52 +0800

    阿里云-印刷文字识别-身份证识别，https://market.aliyun.com/products/57124001/cmapi010401.html?spm=5176.2020520132.101.15.56197218pOHaPK#sku=yuncode440100000

--- src/main/java/cn/happyloves/ali/tools/OcrUtils.java
+++ src/main/java/cn/happyloves/ali/tools/OcrUtils.java
@@ -1,5 +1,56 @@
-package cn.happyloves.ali.tools;/**
+package cn.happyloves.ali.tools;
+
+import cn.hutool.http.HttpRequest;
+import cn.hutool.json.JSONObject;
+import cn.hutool.json.JSONUtil;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
  * @author zc
  * @date 2021/7/7 23:00
- */public class OCRUtils {
+ */
+@Slf4j
+public final class OcrUtils {
+
+    public static void main(String[] args) {
+        System.out.println(idCard("xxxx", "xxxx", false));
+    }
+
+    /**
+     * @param img     图片二进制数据的base64编码/图片url
+     * @param appcode 应用Code
+     * @param isFront 是否是正面
+     * @return 返回值
+     */
+    public static String idCard(String img, String appcode, boolean isFront) {
+        String host = "http://dm-51.data.aliyun.com";
+        String path = "/rest/160601/ocr/ocr_idcard.json";
+        Map<String, String> headers = new HashMap<>(4);
+        headers.put("Authorization", "APPCODE " + appcode);
+        headers.put("Content-Type", "application/json; charset=UTF-8");
+        JSONObject bodyJson = JSONUtil.createObj()
+                .putOpt("image", img)
+                .putOpt("configure", JSONUtil.createObj()
+                        .putOpt("side", isFront ? "face" : "back")
+                        .putOpt("ignore_exif", "false"));
+        //链式构建请求
+        final String body = HttpRequest.post(host + path)
+                .headerMap(headers, true)
+                .body(bodyJson.toString())
+                .timeout(20000)
+                .execute().body();
+        if (JSONUtil.isJson(body)) {
+            JSONObject jsonObject = JSONUtil.parseObj(body);
+            final Boolean success = jsonObject.getBool("success");
+            if (!success) {
+                log.error(body);
+            }
+        } else {
+            log.error(body);
+        }
+        return body;
+    }
 }
